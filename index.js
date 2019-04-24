da430a1aa46f8e46cad1b6922c3babae0e25956d
Pedro <pedrodmbc@gmail.com>
2019-04-24 07:20:22 -0300

    Instalação do ExpressJS

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const http = require("http");
+const express = require("express");
 
-http
-  .createServer((req, res) => {
-    console.log(req);
-    return res.end("hello world");
-  })
-  .listen(3000);
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send(`Welcome ${req.query.name}`);
+});
+
+// app.get("/nome/:name", (req, res) => {
+//   return res.send(`Bem vindo, ${req.params.name}`);
+// });
+app.get("/nome/:name", (req, res) => {
+  return res.json({
+    message: `Bem-vindo, ${req.params.name}`
+  });
+});
+app.listen(3000);
