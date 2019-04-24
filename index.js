d3dfae539f87e6340e658e2630515f2838df2fd6
Pedro <pedrodmbc@gmail.com>
2019-04-24 08:12:58 -0300

    Middlewares - fluxo de requisicoes

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  req.appName = "GoNode";
+
+  return next();
+};
+
+app.use(logMiddleware);
+
 app.get("/", (req, res) => {
   return res.send(`Welcome ${req.query.name}`);
 });
 
-// app.get("/nome/:name", (req, res) => {
-//   return res.send(`Bem vindo, ${req.params.name}`);
-// });
 app.get("/nome/:name", (req, res) => {
   return res.json({
-    message: `Bem-vindo, ${req.params.name}`
+    message: `Bem-vindo ao ${req.appName}, ${req.params.name}`
   });
 });
 app.listen(3000);
